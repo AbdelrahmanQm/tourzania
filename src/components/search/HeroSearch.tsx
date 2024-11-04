33d143a5ed811d0f164d52f312b886aaaf4ffafb
AbdelrahmanQm <abdelrahman.qassem@gmail.com>
2024-11-04 06:00:40 +0200

    working on search on hero section

--- src/components/search/HeroSearch.tsx
+++ src/components/search/HeroSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { SearchIcon } from "../../../public/svgs/page";
+import { CalendarIcon, SearchIcon } from "../../../public/svgs/page";
 
 const HeroSearch: React.FC = () => {
   const [offsetY, setOffsetY] = useState<number>(0);
@@ -13,21 +13,36 @@ const HeroSearch: React.FC = () => {
   });
   return (
     <div
-      className="h-[110px] w-[1000px] bg-white shadow-xl rounded-xl "
+      className="h-[110px] w-[1000px] bg-white shadow-xl rounded-xl flex items-center justify-between px-8"
       style={{ transform: `scale(${offsetY * 0.0003 + 1})` }}
     >
-      <div className="flex items-center gap-2 h-full px-4">
-        <SearchIcon className="text-4xl" />
+      <div className="flex items-center gap-2 h-full  ">
+        <SearchIcon className="text-4xl text-lightgreen" />
         <div className="flex flex-col">
-          <p>Search</p>
+          <p className="font-bold">Search</p>
           <input
             type="text"
             name="search"
             placeholder="Tour, Place, City or Category"
+            className="border-b-2 focus:outline-none w-[300px]"
           />
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center gap-2 h-full  ">
+        <CalendarIcon className="text-4xl text-lightgreen" />
+        <div className="flex flex-col">
+          <p className="font-bold">Your Dates</p>
+          <input
+            type="text"
+            name="search"
+            placeholder="Tour, Place, City or Category"
+            className="border-b-2 focus:outline-none w-[300px]"
+          />
+        </div>
+      </div>
+      <button className="px-8 py-4 font-bold rounded-md bg-lightgreen text-white hover:bg-lightestgreen transition-all duration-500 ">
+        Search
+      </button>
     </div>
   );
 };
